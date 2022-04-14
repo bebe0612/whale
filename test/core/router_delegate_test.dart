ca74ddf9438dbc94bb8f670df89f1da8b077a548
bebe <leebebe0612@gmail.com>
2022-04-14 16:06:29 +0900

    test : Add test for `route_delegate`

---
+++ test/core/router_delegate_test.dart
@@ -0,0 +1,104 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:whale/src/core/page_config.dart';
+import 'package:whale/src/core/router_delegate.dart';
+
+import '../mock/test_view.dart';
+
+void main() {
+  group('Push Test', () {
+    WhaleRouterDelegate routerDelegate = WhaleRouterDelegate(
+        initialPage: PageConfig(name: '/home', view: const HomeView()));
+    test('initialView', () {
+      expect('/home', routerDelegate.getPath());
+    });
+
+    test('push', () {
+      routerDelegate.push(
+          targetPageKey: '/home',
+          pushedPageConfig: PageConfig(name: '/user', view: const UserView()));
+
+      expect('/home/user', routerDelegate.getPath());
+    });
+
+    test('push same name will fail', () {
+      routerDelegate.push(
+          targetPageKey: '/home',
+          pushedPageConfig: PageConfig(name: '/user', view: const UserView()));
+
+      expect('/home/user', routerDelegate.getPath());
+    });
+
+    test('push to target view', () {
+      routerDelegate.push(
+          targetPageKey: '/home',
+          pushedPageConfig:
+              PageConfig(name: '/setting', view: const SettingView()));
+
+      expect('/home/setting/user', routerDelegate.getPath());
+    });
+
+    test('pop target view', () {
+      routerDelegate.pop(viewName: '/setting');
+
+      expect('/home/user', routerDelegate.getPath());
+
+      routerDelegate.pop(viewName: '/user');
+
+      expect('/home', routerDelegate.getPath());
+    });
+
+    test('replace', () {
+      routerDelegate.replace(
+          '/home', PageConfig(name: '/user', view: const UserView()));
+
+      expect('/user', routerDelegate.getPath());
+
+      routerDelegate.replace(
+          '/user', PageConfig(name: '/home', view: const HomeView()));
+
+      expect('/home', routerDelegate.getPath());
+    });
+
+    test('push all', () {
+      routerDelegate.pushAll([
+        PageConfig(
+          name: '/user',
+          view: const UserView(),
+        ),
+        PageConfig(
+          name: '/setting',
+          view: const SettingView(),
+        ),
+      ]);
+
+      expect('/home/user/setting', routerDelegate.getPath());
+    });
+
+    test('replace all', () {
+      routerDelegate.replaceAll([
+        PageConfig(
+          name: '/user',
+          view: const UserView(),
+        ),
+        PageConfig(
+          name: '/setting',
+          view: const SettingView(),
+        ),
+      ]);
+
+      expect('/user/setting', routerDelegate.getPath());
+    });
+
+    test('pop until', () {
+      routerDelegate.push(
+          targetPageKey: '/setting',
+          pushedPageConfig: PageConfig(name: '/home', view: const HomeView()));
+
+      expect('/user/setting/home', routerDelegate.getPath());
+
+      routerDelegate.popUntil(viewName: '/user');
+
+      expect('/user', routerDelegate.getPath());
+    });
+  });
+}
---
+++ test/mock/test_view.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class UserView extends StatelessWidget {
+  const UserView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+class SettingView extends StatelessWidget {
+  const SettingView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
